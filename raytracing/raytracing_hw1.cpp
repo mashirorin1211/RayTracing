2622c6223574fb8d4e627d61a6092f04f7f83f7c
MashiroRin <45223205+mashirorin1211@users.noreply.github.com>
2023-09-21 18:42:22 +0800

    Add files via upload

--- raytracing/raytracing_hw1.cpp
+++ raytracing/raytracing_hw1.cpp
@@ -1,11 +1,46 @@
-#include <stdio.h>
+#include<iostream>
+#include <string>
 #include <fstream>
 #include <fstream>
 #include <sstream>
+#include "algebra3.h"
+
 using namespace std;
+
+int eyes[3];
+ifstream in;
+
+void ReadData(){
+
+
+    string s;
+
+    in.open("hw1_input.txt"); 
+
+    if (in.fail()){  
+        cout << "Error opening file" ;
+        exit(1);
+    }
+    else{
+        //printf("File opened successfully\n");
+    }
+
+
+}
+
+void Circle_raytraing(int E[],int O[],int R[],int S[]){
+
+
+}
+
+
 
 
 int main(){
 
+    ReadData();
 
+
+    in.close();
+    return 0;
 }
